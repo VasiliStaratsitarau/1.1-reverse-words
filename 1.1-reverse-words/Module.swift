dc04d05b80dde31430c1fc401c6633da5f464536
Vasili Staratsitarau <v.staratsitarau@gmail.com>
2022-05-22 20:19:10 +0200

    all edits complete

--- 1.1-reverse-words/Module.swift
+++ 1.1-reverse-words/Module.swift
@@ -8,26 +8,6 @@
 import Foundation
 
 func reverseWordsModule(text: String) -> String {
-        let reverseWords = text.split(separator: " ").map { String($0.reversed())}.joined(separator: " ")
-        return reverseWords
+    let reverseWords = text.split(separator: " ").map { String($0.reversed())}.joined(separator: " ")
+    return reverseWords
 }
-//var buttonCounter = 0
-//@IBAction func reverseButton(_ sender: ChildButton) {
-//    buttonCounter += 1
-//    switch buttonCounter {
-//    case 1:
-//        if let text = inputText.text {
-//            reverseTextView.text = reverseWordsModule(text: text)
-//            reverseButton.setTitle("Clear", for: .normal)
-//        }
-//    case 2:
-//        if reverseTextView.text != "" {
-//            inputText.text = ""
-//            reverseTextView.text = ""
-//            reverseButton.buttonDisable()
-//            reverseButton.setTitle("REVERSE IT!", for: .normal)
-//            buttonCounter = 0
-//        }
-//    default: break
-//    }
-//}
